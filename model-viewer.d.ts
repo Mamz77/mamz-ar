e92e8c56990446f2739130f4c12f8bcf7a32fb14
mamz77 <mohammadreza.shirali77@gmail.com>
2026-07-19 13:09:28 +0330

    Fix model-viewer TypeScript type

--- model-viewer.d.ts
+++ model-viewer.d.ts
@@ -1,26 +1,25 @@
-import { DetailedHTMLProps, HTMLAttributes } from "react";
+import type { DetailedHTMLProps, HTMLAttributes } from "react";
 
-declare global {
+type ModelViewerAttributes = HTMLAttributes<HTMLElement> & {
+  src?: string;
+  "ios-src"?: string;
+  alt?: string;
+  poster?: string;
+  ar?: boolean;
+  "ar-modes"?: string;
+  "ar-placement"?: string;
+  "camera-controls"?: boolean;
+  "auto-rotate"?: boolean;
+  "shadow-intensity"?: string;
+  "shadow-softness"?: string;
+  "environment-image"?: string;
+  exposure?: string;
+};
+
+declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
-      "model-viewer": DetailedHTMLProps<
-        HTMLAttributes<HTMLElement> & {
-          src?: string;
-          "ios-src"?: string;
-          alt?: string;
-          poster?: string;
-          ar?: boolean;
-          "ar-modes"?: string;
-          "ar-placement"?: string;
-          "camera-controls"?: boolean;
-          "auto-rotate"?: boolean;
-          "shadow-intensity"?: string;
-          "shadow-softness"?: string;
-          "environment-image"?: string;
-          exposure?: string;
-        },
-        HTMLElement
-      >;
+      "model-viewer": DetailedHTMLProps<ModelViewerAttributes, HTMLElement>;
     }
   }
 }
